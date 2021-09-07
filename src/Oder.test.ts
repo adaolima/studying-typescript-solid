ffd05766796d8c2e9ddbeb51df5fa35be9ac082d
Adão Lima <adaoolima@gmail.com>
2021-09-07 00:54:45 -0300

    Added DI

--- src/Oder.test.ts
+++ src/Oder.test.ts
@@ -1,11 +1,12 @@
 import Beer from "./Beer";
+import MessageDataFile from "./MessageDataFile";
 import Order from "./Order";
 import Water from "./Water";
 import Whisky from "./Whisk";
 
 test("Should create an order and calculate the total", function () {
 
-  const order = new Order();
+  const order = new Order(new MessageDataFile());
   order.addItem(new Beer("Brahma", 10));
   order.addItem(new Whisky("Jack Daniels", 100));
   order.addItem(new Water("Crystal", 1));
@@ -17,7 +18,7 @@ test("Should create an order and calculate the total", function () {
 
 test("Should create an order and calculate the taxes", function () {
 
-  const order = new Order();
+  const order = new Order(new MessageDataFile());
   order.addItem(new Beer("Brahma", 10)); // 10%
   order.addItem(new Whisky("Jack Daniels", 100)); //20%
   order.addItem(new Water("Crystal", 1)); // no taxes
@@ -25,4 +26,28 @@ test("Should create an order and calculate the taxes", function () {
   const taxes= order.getTaxes();
 
   expect(taxes).toBe(21);
+});
+
+test("Should create an order and print a message in english", async function () {
+
+  const order = new Order(new MessageDataFile());
+  order.addItem(new Beer("Brahma", 10)); // 10%
+  order.addItem(new Whisky("Jack Daniels", 100)); //20%
+  order.addItem(new Water("Crystal", 1)); // no taxes
+
+  const message = await order.printMessage('en');
+
+  expect(message).toBe("The total is US$ 111, and the taxes are US$ 21. Thank you, for your purchase!");
+});
+
+test("Should create an order and print a message in portuguese", async function () {
+
+  const order = new Order(new MessageDataFile());
+  order.addItem(new Beer("Brahma", 10)); // 10%
+  order.addItem(new Whisky("Jack Daniels", 100)); //20%
+  order.addItem(new Water("Crystal", 1)); // no taxes
+
+  const message = await order.printMessage('pt');
+
+  expect(message).toBe("O total é de R$ 111, e as taxas são R$ 21. Obrigado pela compra!");
 });
